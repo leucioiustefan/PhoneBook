f4b1793bedcacc0d51a285e7dd83cb779fa94a9a
Stefan Leucioiu <50478258+StormriderX@users.noreply.github.com>
2020-05-17 11:39:49 +0300

    commit

--- users.js
+++ users.js
@@ -1,14 +1,3 @@
-const tbodyUsersEl = document.getElementById('users');
-const submitBtnEl = document.getElementById('submit');
-const fullNameEl = document.getElementById('name');
-const phoneNumberEl = document.getElementById('number');
-const emailEl = document.getElementById('email');
-const formEl = document.querySelector('form');
-const regexName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-const regexEmail = /\S+@\S+\.\S+/;
-const regexNumber = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-const users = [];
-
 class Users {
   isValidUser = false;
 
@@ -96,43 +85,3 @@ class User extends Users {
     }
   }
 }
-
-const resetForm = () => {
-  fullNameEl.value = '';
-  phoneNumberEl.value = '';
-  emailEl.value = '';
-};
-
-const resetName = () => {
-  fullNameEl.value = '';
-};
-
-const resetNumber = () => {
-  phoneNumberEl.value = '';
-};
-
-const resetEmail = () => {
-  emailEl.value = '';
-};
-
-const userObjectHandler = () => {
-  const user = new User(fullNameEl.value, phoneNumberEl.value, emailEl.value);
-  const isDuplicate = users.some((singleUser) =>
-    user.checkForDuplicates(singleUser, user)
-  );
-  if (user.renderUserOnScreen()) {
-    users.push(user);
-  }
-  if (isDuplicate && users.length > 1) {
-    alert('User already exists');
-    const duplicatePerson = tbodyUsersEl.lastElementChild;
-    tbodyUsersEl.removeChild(duplicatePerson);
-    users.pop(user);
-  }
-  console.log(users);
-};
-
-submitBtnEl.addEventListener('click', userObjectHandler);
-formEl.addEventListener('submit', (e) => {
-  e.preventDefault();
-});
